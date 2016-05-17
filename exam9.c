910044dc6f334a945a8775e985f1044fd3e29ba0
Hong-Xiao-Yun <nike12018@yahoo.com.tw>
2016-05-17 10:16:10 +0800

    compute time

--- exam9.c
+++ exam9.c
@@ -33,7 +33,7 @@ void Dequeue(sem s);
 void Destroy(sem s);
 void *active(void *aegp);
 static int REASONABLE_THREAD_MAX=500;
-
+double countTime=0.0;
 
 //initial semaphore
 // int a; a=10
@@ -46,6 +46,20 @@ struct  semaphore global_sem= {
 
 int countNo;
 pthread_mutex_t gLock;
+static double diff_in_second(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec-t1.tv_nsec < 0) {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
+    } else {
+        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec + diff.tv_nsec / 1000000000.0);
+}
+double sumrelease=0.0;
+double sumrequire=0.0;
 int main(int argc, char const *argv[])
 {
     sem s=&global_sem;//pointer to variable
@@ -79,28 +93,48 @@ int main(int argc, char const *argv[])
             fcntl(s->thrs[i].pipe.out, F_SETFL, O_NONBLOCK | O_WRONLY);//set pipe
 
         }
+       
         for (int i = 0; i < nhijos; i++) {
             pthread_create(&tid[i], NULL, &active, threadS[i] );
         }
         for (int i = 0; i < nhijos; i++) {
             pthread_join( tid[i], NULL );
         }
+      
+
     }
+  
+      printf("average acquire time=%1.3f ms\n",sumrequire/nhijos);
+    printf("average require time=%1.3f ms\n",sumrelease/nhijos);
+    // printf("average time=%1.3f ms\n",countTime/nhijos);
     pthread_mutex_destroy(&gLock);
 }
 void *active(void *aegp)
 {
+     struct timespec start, end;
     sem s=&global_sem;//pointer to variable
     threads *arg=(threads *)aegp;
     int no=arg->no;
     int tid=arg->tid;
-
+     clock_gettime(CLOCK_REALTIME, &start);
     s->p(s,tid); //acquire
-    printf("count ~~~~~%d\n",s->count );
+    clock_gettime(CLOCK_REALTIME, &end);
+    fprintf(stderr,"Execution time of acquire() : %lf sec\n", diff_in_second(start, end));
+    sumrequire+=diff_in_second(start, end);
+    // printf("count ~~~~~%d\n",s->count );
     //critical section
     countNo++;
     no=countNo;
+
+     clock_gettime(CLOCK_REALTIME, &start);
+
     s->v(s,tid); //release
+     clock_gettime(CLOCK_REALTIME, &end);
+    fprintf(stderr,"Execution time of release() : %lf sec\n", diff_in_second(start, end));
+   sumrelease+=diff_in_second(start, end);
+         // printf(" time=%1.3f ms\n",(double)(end1-start1)/CLOCKS_PER_SEC*1000);
+        // printf("Execution time of raytracing() : %lf sec\n", diff_in_second(start, end));
+     
     pthread_exit(0);
 }
 //atomic: only one cpu(thread) can do TestAndSet
